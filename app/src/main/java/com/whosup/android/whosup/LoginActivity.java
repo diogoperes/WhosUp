4b1de59b931d828120b98c8bf5bb6b5ff0df5427
DiogoPeres <diogo.peres@hotmail.com>
2015-04-14 19:48:43 +0100

    ueue

--- app/src/main/java/com/whosup/android/whosup/LoginActivity.java
+++ app/src/main/java/com/whosup/android/whosup/LoginActivity.java
@@ -1,9 +1,10 @@
 package com.whosup.android.whosup;
 
-        import android.app.Activity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.TextView;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
     @Override
@@ -12,16 +13,17 @@ public class LoginActivity extends Activity {
         // setting default screen to login.xml
         setContentView(R.layout.activity_login);
 
-        /*TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+        TextView registerScreen = (TextView) findViewById(R.id.signUp);
 
         // Listening to register new account link
         registerScreen.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 // Switching to Register screen
-                //Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-               // startActivity(i);
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+
             }
-        });*/
+        });
     }
 }
