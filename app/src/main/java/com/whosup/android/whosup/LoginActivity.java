59d456684551977e6fd01c9894010506f2afebf4
DiogoPeres <diogo.peres@hotmail.com>
2015-04-16 19:03:24 +0100

    ueue

--- app/src/main/java/com/whosup/android/whosup/LoginActivity.java
+++ app/src/main/java/com/whosup/android/whosup/LoginActivity.java
@@ -1,19 +1,46 @@
 package com.whosup.android.whosup;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends Activity {
+
+    Button buttonLogin;
+    TextView registerScreen;
+    EditText inputEmail;
+    EditText inputPassword;
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // setting default screen to login.xml
         setContentView(R.layout.activity_login);
+        registerScreen = (TextView) findViewById(R.id.signUp);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        buttonLogin = (Button) findViewById(R.id.login);
 
-        TextView registerScreen = (TextView) findViewById(R.id.signUp);
 
         // Listening to register new account link
         registerScreen.setOnClickListener(new View.OnClickListener() {
@@ -26,4 +53,5 @@ public class LoginActivity extends Activity {
             }
         });
     }
+
 }
--- app/src/main/java/com/whosup/android/whosup/RegisterActivity.java
+++ app/src/main/java/com/whosup/android/whosup/RegisterActivity.java
@@ -3,17 +3,65 @@ package com.whosup.android.whosup;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class RegisterActivity extends Activity {
+
+    EditText editTextUserName,editTextUserEmail,editTextPassword,editTextConfirmPassword;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_register);
 
+
         TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+        Button button_register_new_account = (Button) findViewById(R.id.button_register_new_account);
+        editTextUserName=(EditText)findViewById(R.id.editTextUserName);
+        editTextUserEmail=(EditText)findViewById(R.id.editTextUserEmail);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        editTextConfirmPassword=(EditText)findViewById(R.id.editTextConfirmPassword);
+
+        button_register_new_account.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                String userName=editTextUserName.getText().toString();
+                String userEmail=editTextUserEmail.getText().toString();
+                String password=editTextPassword.getText().toString();
+                String confirmPassword=editTextConfirmPassword.getText().toString();
+                // check if any of the fields are vaccant
+                if(userName.equals("")||password.equals("")||confirmPassword.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), R.string.please_insert_all_fields, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(!userEmail.contains("@"))
+                {
+                    Toast.makeText(getApplicationContext(), R.string.invalid_email, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // check if both password matches
+                if(!password.equals(confirmPassword))
+                {
+                    Toast.makeText(getApplicationContext(), R.string.password_does_not_match, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+                    // Save the Data in Database
+                    //loginDataBaseAdapter.insertEntry(userName, password);
+                    Toast.makeText(getApplicationContext(), R.string.account_successfully_created, Toast.LENGTH_LONG).show();
+                    finish();
+                }
+
+
+            }
+        });
 
         // Listening to Login Screen link
         loginScreen.setOnClickListener(new View.OnClickListener() {
