8fce7fa4d36b977881a511325a1e04a3c9eb2b1c
DiogoPeres <diogo.peres@hotmail.com>
2015-04-16 22:43:53 +0100

    drawable

---
+++ app/src/main/java/com/whosup/library/JSONParser.java
@@ -0,0 +1,7 @@
+package com.whosup.library;
+
+/**
+ * Created by DiogoPeres on 16-04-2015.
+ */
+public class JSONParser {
+}
---
+++ app/src/main/java/library/DataBaseHandler.java
@@ -0,0 +1,7 @@
+package library;
+
+/**
+ * Created by DiogoPeres on 16-04-2015.
+ */
+public class DataBaseHandler {
+}
