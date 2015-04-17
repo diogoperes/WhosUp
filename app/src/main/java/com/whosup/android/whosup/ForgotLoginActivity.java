6db1ab84ac340aefff2686fec5b6323b760e2888
NunoCoelho <nuno.bettencourt.coelho@gmail.com>
2015-04-17 13:37:54 +0100

    recovery password completed

--- app/src/main/java/com/whosup/android/whosup/ForgotLoginActivity.java
+++ app/src/main/java/com/whosup/android/whosup/ForgotLoginActivity.java
@@ -3,21 +3,37 @@ package com.whosup.android.whosup;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForgotLoginActivity extends Activity{
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    private static final String RECOVER_LOGIN_URL = "http://whosup.host22.com/recover_login.php";
 
     Button buttonLogin;
     TextView buttonRecover, loginscreen;
-    EditText inputEmail;
+    EditText email;
+    JSONParser jsonParser = new JSONParser();
     private ProgressDialog pDialog;
-    public static final String ok = "Login retrieved. Please check your email.";
-    public static final String fail = "Login could not be retrieved. Please try again";
+    public static final String ok = "Password retrieved. Please check your email.";
+    public static final String fail = "Password could not be retrieved. Please try again";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +41,7 @@ public class ForgotLoginActivity extends Activity{
         // setting default screen to login.xml
         setContentView(R.layout.activity_forgot_login);
         loginscreen = (TextView) findViewById(R.id.login);
-        inputEmail = (EditText) findViewById(R.id.email);
+        email = (EditText) findViewById(R.id.email);
         buttonRecover = (Button) findViewById(R.id.retrieveLogin);
 
 
@@ -33,12 +49,12 @@ public class ForgotLoginActivity extends Activity{
 
             @Override
             public void onClick(View v) {
-                String email = inputEmail.getText().toString();
-                if(email.equals("") || !email.contains("@") || !email.contains(".")){
+                String e = email.getText().toString();
+                if(e.equals("") || !e.contains("@") || !e.contains(".")){
                     Toast.makeText(getApplicationContext(), R.string.invalid_email, Toast.LENGTH_LONG).show();
                     return;
                 }
-                new RecoverLoginProgress(ForgotLoginActivity.this, "Retrieving your login. Please wait...", ok, fail, email).execute();
+                new AttemptRecoveryLogin().execute();
 
             }
         });
@@ -51,6 +67,70 @@ public class ForgotLoginActivity extends Activity{
                 finish();
             }
         });
+    }
+
+    class AttemptRecoveryLogin extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(ForgotLoginActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            int success;
+            String eMail = email.getText().toString();
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("email", eMail));
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(
+                        RECOVER_LOGIN_URL, "POST", params);
+
+                // check your log for json response
+                if(json == null){
+                    System.err.println("DOR");
+                }
+                Log.d("Recovery login attempt", json.toString());
+
+                // json success tag
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Recovery login successful!", json.toString());
+                    return ok;
+                }else{
+                    Log.d("Recovery login Failure!", json.getString(TAG_MESSAGE));
+                    return fail;
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        protected void onPostExecute(String s) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (s.equals(fail)) {
+                Toast.makeText(ForgotLoginActivity.this, s, Toast.LENGTH_LONG).show();
+            }
+            if (s.equals(ok)) {
+                Toast.makeText(ForgotLoginActivity.this, s, Toast.LENGTH_LONG).show();
+                finish();
+            }
+
+        }
     }
 
 }
